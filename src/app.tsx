3a77e3ddc442ef0e826888ae204dca8a44aa4d96
LuizHLG <luizlelek08@gmail.com>
2024-06-23 19:05:31 -0300

    fix: correction in list footer

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,12 @@ import { Form } from "./components/Form";
 import { Header } from "./components/Header";
 import { TodoList } from "./components/TodoList";
 import { Footer } from "./components/Footer";
+import { useContext } from "react";
+import { TodoContext } from "./providers/TodoContext";
 
 export default function App() {
+
+  const { backupTodoList } = useContext(TodoContext)
 
   return (
     <>
@@ -13,8 +17,12 @@ export default function App() {
         <Form />
         <TodoList />
       </main>
-      <div className="background1"></div>
-      <div className="background2"></div>
+      {backupTodoList.length > 0 && (
+        <>
+          <div className="background1"></div>
+          <div className="background2"></div>
+        </>
+      )}
       <Footer />
     </>
   );
